56dec20079f05565d465d883319aac820e904385
zainmasood22ltsb <154008201+zainmasood22ltsb@users.noreply.github.com>
2025-04-08 10:41:30 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,64 @@
+let a = "hell oworld"
+let zain = ()=>{
+    let ban = "zeuno"
+}
+console.log(a);
+console.log(ban);
+for(var i =0 ; i<=6 ; i++){
+    console.log(i);
+}
+console.log(i);
+var sirf function main local hai agar kai par bhi hohun tu challley so variable is function scope hai 
+
+
+
+
+
+// function abc (){
+
+// }
+
+
+
+//let sirf block scope hai 
+hoisting 
+console.log(k);
+
+var k = "jst"
+tdz temporal dead zone 
+
+
+// sabsey pehlay download hoita hai html pir cs s and javascript
+
+
+
+
+
+// switch case
+if(day == "sunday" || day == "monday"){
+    console.log("weekend");
+}
+else if (day == "saturday" || day == "wednesday"){
+    console.log("week");
+}
+else{
+    console.log("bhago");
+    
+}
+var i =0 
+while (i<5){
+    i++
+    console.log(i);
+    
+}
+// yeh jab chaaalo jab no of itenrtions kan iap aat ahohohun 
+
+// while loop
+var arr = ["kahi","lah","wed"]
+var 
+// ismain pehlay code run hota hai pir nconditon hoti hai do while main 
+// async vs defer 
+// sweet alert 
+//html parsing
+
+
